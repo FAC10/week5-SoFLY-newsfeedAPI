d464b5797f9f2a0ee2e090598fa01e13c803afc9
Samatar Axmed <Samatar_267@hotmail.com>
2017-03-22 15:17:31 +0000

    Made DailyMail API-Call Weve made a succesfull Dailymail api call which returns the title, description, url and thumbnail. Relates to issue #20

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,1 +1,17 @@
 console.log('Hi, its working');
+var fetch = function(url, cb) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        cb(null, JSON.parse(xhr.responseText));
+      } else {
+        cb(true);
+      }
+    }
+  };
+  xhr.open('GET', url, true);
+  xhr.send();
+};
+
+fetch( 'https://newsapi.org/v1/articles?source=daily-mail&sortBy=top&apiKey=c0f475f6de2d42998de013309ad9d70b', console.log)
--- testapi_dailymail.js
+++ testapi_dailymail.js
@@ -3,7 +3,26 @@ const request = require('request');
 const API_KEY = process.env.dailymail_api;
 const url = `https://newsapi.org/v1/articles?source=daily-mail&sortBy=top&apiKey=${API_KEY}`;
 
-request(url, function(error, response, body){
-console.log(API_KEY);
-  console.log(body.status);
+request(url, (error, response, body)=>{
+  const mail = JSON.parse(body);
+  const output = [];
+
+  mail.articles.forEach(e => {
+    const holder = {};
+    if(e.title){
+      holder.title = e.title;
+    }
+    if(e.url){
+      holder.url = e.url;
+    }
+    if (e.description){
+      holder.description = e.description;
+    }
+    if(e.urlToImage){
+      holder.thumbnail = e.urlToImage;
+    }
+    output.push(holder);
+  });
+
+  console.log(output);
 });
