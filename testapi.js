490398d3146eb6539a6fdf7cb5eb6396258b9905
Yvonne Liu <liuyvonne.ca@gmail.com>
2017-03-22 15:01:11 +0000

    add word length to summary result, add error handling, wrap function in module, add search term
    relates to #1

--- testapi.js
+++ testapi.js
@@ -1,37 +1,72 @@
 const env = require('env2')('./.env');
 const request = require('request');
 
-const guardianApiUrl = 'https://content.guardianapis.com/search?q=politics&show-blocks=body,main&show-fields=thumbnail' + process.env.guardian_api;
+const guardianFetch = (searchterm, callback) => {
 
-request(guardianApiUrl, (err, response, body) => {
-  const articles = JSON.parse(body);
+  const guardianPath = 'https://content.guardianapis.com/search?q=';
+  const endQueries = '&show-blocks=body,main&show-fields=thumbnail';
+  const guardianApiKey = process.env.guardian_api;
 
-  const output = [];
+  const guardianApiUrl = guardianPath + searchterm + endQueries + guardianApiKey;
 
-  if (articles.response && articles.response.results) {
-    console.log('working');
-
-    articles.response.results.forEach(e => {
-      const article = {};
-      if (e.webTitle) {
-        article.title = e.webTitle;
-      }
-      if (e.webUrl) {
-        article.url = e.webUrl;
-      }
-      if (e.blocks && e.blocks.body && e.blocks.body[0].bodyTextSummary) {
-        article.sumary = e.blocks.body[0].bodyTextSummary; //@TODO: add a function here that cuts off the summary
-      }
-      if (e.fields && e.fields.thumbnail) {
-        article.thumbnail = e.fields.thumbnail;
-      }
-      output.push(article);
-    });
+  function cutOffSummary(string, len = 50) {
+    const words = string.split(' ');
+    if (words.length >= len) {
+      return words.slice(0, len).join(' ') + '...';
+    }
+    return string;
   }
 
-  //@TODO: Limit search results to 5-10 articles
-  //@TODO: Add error handling
+  function buildArticle(e) {
+    const article = {};
+    if (e.webTitle) {
+      article.title = e.webTitle;
+    }
+    if (e.webUrl) {
+      article.url = e.webUrl;
+    }
+    if (e.blocks && e.blocks.body && e.blocks.body[0].bodyTextSummary) {
+      article.sumary = cutOffSummary(e.blocks.body[0].bodyTextSummary);
+    }
+    if (e.fields && e.fields.thumbnail) {
+      article.thumbnail = e.fields.thumbnail;
+    }
 
-  console.log(output);
-  // console.log(body);
+    return Object.keys(article).length === 4 ?
+      article :
+      null;
+  }
+
+
+  request(guardianApiUrl, (err, response, body) => {
+    const articles = JSON.parse(body);
+
+    const output = [];
+    if (err) {
+      callback(err);
+    }
+    if (articles.response && articles.response.results) {
+      console.log('working');
+
+      articles.response.results.forEach(e => {
+        if (output.length < 5) {
+          const article = buildArticle(e);
+          if (article) {
+            output.push(article);
+          }
+        }
+      });
+    }
+    if (output.length) {
+      callback(null, output);
+    } else {
+      callback(new Error('No search results'));
+    }
+
+  });
+
+};
+
+guardianFetch('trump', (err, result)=>{
+  console.log(result);
 });
