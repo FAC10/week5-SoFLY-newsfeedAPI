158a6842b39dbe66e23f2d7de48e54e8ca7d9545
finnhodgkin <finnhodgkin@gmail.com>
2017-03-23 10:18:39 +0000

    abstract out guardian functions for testing, add a shot route testing file add api call section to router/handler relates to #26, #9

--- src/router.js
+++ src/router.js
@@ -1,24 +1,30 @@
-var handler = require('./handler.js');
+const handler = require('./handler.js');
+const _url = require('url');
 
-module.exports = function(request, response){
-  var url = request.url;
-  console.log(url);
-  var page = {
+module.exports = (req, res) => {
+  const url = req.url;
+  const pathname = _url.parse(url).pathname;
+
+  const page = {
     '/' : 'index.html'
-    //ADD PAGES HERE
   }[url];
 
+  const api = {
+    '/search' : handler.search
+  }[pathname];
+
+  // ROUTES:
   if (page) {
-    handler.serveStatic(request, response, page);
-  }
+    handler.serveStatic(req, res, page);
 
-  else if (url.indexOf('/assets') === 0) {
-    handler.servePublic(request, response);
+  } else if (api) {
+    api(req, res);
 
-  }
+  } else if (url.indexOf('/assets') === 0) {
+    handler.servePublic(req, res);
 
-  else {
-    handler.serveError(request, response);
+  } else {
+    handler.serveError(req, res);
 
   }
 };
--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,6 @@
 const test = require('tape');
 const backendTests = require('./backend/backendtests.js');
+const routeTests = require('./backend/route-tests.js');
 // const frontendTests = require('./frontend/frontendtest.js');
 
 test('Check tape is working with a simple passing test', (t) => {
@@ -8,4 +9,5 @@ test('Check tape is working with a simple passing test', (t) => {
 });
 
 backendTests();
+routeTests();
 // frontendTests();
