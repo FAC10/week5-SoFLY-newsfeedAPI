6ddbc7cd118d864fbeed87afdb8a6e86366be47b
Finn Hodgkin <finnhodgkin@gmail.com>
2017-03-23 15:23:15 +0000

    Re-add backend tests

--- test/tests.js
+++ test/tests.js
@@ -3,11 +3,11 @@ const backendTests = require('./backend/backendtests.js');
 const routeTests = require('./backend/route-tests.js');
 // const frontendTests = require('./frontend/frontendtest.js');
 
-// test('Check tape is working with a simple passing test', (t) => {
-//   t.pass('a message to print out on sucess');
-//   t.end();
-// });
-//
-// backendTests();
+test('Check tape is working with a simple passing test', (t) => {
+  t.pass('a message to print out on sucess');
+  t.end();
+});
+
+backendTests();
 routeTests();
 // frontendTests();
